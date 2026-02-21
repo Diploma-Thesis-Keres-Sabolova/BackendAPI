feced7030f7284a3242877c7d7effbd6c6c4483d
ABS\fikesk <fikesk@infoconsulting.eu>
2026-02-21 10:57:38 +0100

    fixed errors with inconsistent models

--- DataGathering/DataClasses/RawDataInfo.py
+++ DataGathering/DataClasses/RawDataInfo.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from dataclasses import dataclass
-from typing import Optional, Dict, Any
+from typing import Dict, Any
 
 
 @dataclass
 class RawDataInfo:
     id: int
+    run_id: int
     data: Dict[str, Any]
+    created_at: datetime
