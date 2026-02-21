b1cae57b3d6fd666ae139ffb57517dd2fc1c5fd2
ABS\fikesk <fikesk@infoconsulting.eu>
2026-02-21 19:57:20 +0100

    fixed minor mistakes

--- app/schemas/RawDataSchema.py
+++ app/schemas/RawDataSchema.py
@@ -5,7 +5,7 @@ from .CustomBaseModel import CustomBaseModel
 class RawDataResponse(CustomBaseModel):
     id: int
     run_id: int
-    data: Dict[str, Any]
+    data: Any
     created_at: datetime
 
     class Config(CustomBaseModel.Config):
@@ -14,15 +14,15 @@ class RawDataResponse(CustomBaseModel):
 
 class RawDataInRun(CustomBaseModel):
     id: int
-    data: Dict[str, Any]
+    data: Any
 
     class Config(CustomBaseModel.Config):
         from_attributes = True
 
 class RawDataCreate(CustomBaseModel):
     run_id: int
-    data: Dict[str, Any]
+    data: Any
 
 
 class RawDataUpdate(CustomBaseModel):
-    data: Optional[Dict[str, Any]] = None
+    data: Any = None
