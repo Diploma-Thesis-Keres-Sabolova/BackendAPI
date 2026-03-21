7db2af4dc1c0e4a658b3f7017b866d9e73426c00
ABS\fikesk <fikesk@infoconsulting.eu>
2026-03-21 12:21:31 +0100

    new endpoint for prediction model purposes

--- app/api/processed_data.py
+++ app/api/processed_data.py
@@ -1,19 +1,74 @@
+from datetime import datetime
+
 from fastapi import APIRouter, Depends, Query
-from sqlalchemy.orm import Session
-from typing import List
+from sqlalchemy.orm import Session, joinedload
+from typing import List, Optional
 
 from app.database import get_db
 from app.dependencies.auth import get_api_key
 from app.dependencies.processed_data_get_filters import ProcessedDataFilter
 from app.models.ProcessedData import ProcessedData
+from app.models.Run import Run
 from app.repositories.processed_data_repository import apply_processed_data_filters
-from app.schemas.ProcessedDataSchema import ProcessedDataResponse, ProcessedDataCreate, ProcessedDataUpdate
+from app.schemas.ProcessedDataSchema import ProcessedDataResponse, ProcessedDataCreate, ProcessedDataUpdate, \
+    ProcessedDataWithRunResponse
 
 router = APIRouter(
     prefix="/processed_data",
     tags=["ProcessedData"],
     dependencies=[Depends(get_api_key)]
 )
+
+@router.get("/latest", response_model=List[ProcessedDataWithRunResponse])
+def get_latest_data_for_model(
+    db: Session = Depends(get_db),
+    provider_id: Optional[int] = Query(None),
+    limit: int = Query(3000, le=50000),
+):
+    query = db.query(ProcessedData)
+
+    if provider_id is not None:
+        query = query.join(Run).filter(Run.provider_id == provider_id)
+
+    query = query.options(joinedload(ProcessedData.run))
+
+    return (
+        query
+        .order_by(ProcessedData.timestamp.desc())
+        .limit(limit)
+        .all()
+    )
+
+
+@router.get("/range", response_model=List[ProcessedDataWithRunResponse])
+def get_data_by_date_range(
+        start_date: datetime = Query(..., description="Start Interval"),
+        end_date: datetime = Query(..., description="End Interval"),
+        provider_id: Optional[int] = Query(None, description="Provider"),
+        limit: int = Query(3000, le=50000, description="Max cnumber of returned data"),
+        offset: int = Query(0, description="Offset"),
+        db: Session = Depends(get_db)
+):
+
+    query = db.query(ProcessedData)
+
+    query = query.filter(
+        ProcessedData.timestamp >= start_date,
+        ProcessedData.timestamp <= end_date
+    )
+
+    if provider_id is not None:
+        query = query.join(Run).filter(Run.provider_id == provider_id)
+
+    query = query.options(joinedload(ProcessedData.run))
+
+    return (
+        query
+        .order_by(ProcessedData.timestamp.desc())
+        .offset(offset)
+        .limit(limit)
+        .all()
+    )
 
 @router.get("/", response_model=List[ProcessedDataResponse])
 def read_processed_data(
